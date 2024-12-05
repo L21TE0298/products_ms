89bed61ff40c6a8bd2d27b76c84463ce43e9ddec
L21TE0298 <l21te0298@teziutlan.tecnm.mx>
2024-12-05 07:48:58 -0600

    2nd DEPLOY

--- src/main/java/com/ghostappi/backend/config/SecurityConfig.java
+++ src/main/java/com/ghostappi/backend/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests(
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
                 auth -> auth.requestMatchers("/signin", "/signup").permitAll()
                         .requestMatchers("/nutrients/**", "/products/**").hasRole("ADMIN")
                         .requestMatchers("/wallets/**", "/cards/**").hasRole("USER")
